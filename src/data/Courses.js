a9025d2bc0dae2d8bf90ab285c58d9a095532137
Pradeep Binwal <Pradeepbinwal1@gmail.com>
2024-03-07 09:09:46 +0530

    first

---
+++ src/data/Courses.js
@@ -0,0 +1,65 @@
+export const courses = [
+    {
+      id: 1,
+      title: 'The Complete Android Development Bootcamp',
+      category:'Android',
+      rating:5,
+      price: '₹159.99',
+      linkImg:
+      'https://i.ytimg.com/vi/Iz08OTTjR04/maxresdefault.jpg'
+    },
+    {
+      id: 2,
+      title: 'The Complete 2023 Web Development Bootcamp',
+      category:'Web Dev',
+      rating:4,
+      price: '₹159.99',
+      linkImg:
+        'https://tse4.mm.bing.net/th?id=OIP.V-Jp13LvtVc2IiY2fp4qYwHaD4&pid=Api&P=0&h=220'
+      },
+    {
+      id: 3,
+      title: 'Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!',
+      category:'Python',
+      rating:4,
+      price: '₹159.99',
+      linkImg:
+        'https://tse2.mm.bing.net/th?id=OIP.w3nlH550zf4k4yy94SdMiwHaE9&pid=Api&P=0&h=220'
+       },
+    {
+      id: 4,
+      title: 'The Complete JavaScript Course 2023: From Zero to Expert!',
+      category:'Javascript',
+      rating:4,
+      price: '₹159.99',
+      linkImg:
+        'https://tse2.mm.bing.net/th?id=OIP.y20z6j6diGgdYvbr9qBVRQHaEK&pid=Api&P=0&h=220'
+       },
+    {
+      id: 5,
+      title: 'HTML',
+      category:'Html',
+      rating:4,
+      price: '₹119.99',
+      linkImg:
+        'https://tse1.mm.bing.net/th?id=OIP.2rjfKIzdYUIf817e19myPQHaEK&pid=Api&P=0&h=220'
+       },
+    {
+      id: 6,
+      title: 'CSS',
+      category:'css',
+      rating:4,
+      price: '₹129.99',
+      linkImg:
+        'https://i.ytimg.com/vi/ywBg3YV2PJU/maxresdefault.jpg'
+       },
+    {
+      id: 7,
+      title: 'ReactJs',
+      category:'react',
+      rating:4,
+      price: '₹159.99',
+      linkImg:
+        'https://tse1.mm.bing.net/th?id=OIP.SqM_nDj6apfGFE_QQREgrQHaEK&pid=Api&P=0&h=220'
+       },
+  ];
